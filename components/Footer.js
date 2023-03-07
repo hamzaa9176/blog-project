24f9dbcb51d60ec90913c9f31d7c128d4046dd0d
Hamza Ali <hamzaali@Air-di-Hamza.lan>
2023-03-07 20:05:31 +0100

    final commit but design needs improvments alonside color schema

--- components/Footer.js
+++ components/Footer.js
@@ -4,7 +4,7 @@ import Link from 'next/link'
 const Footer = () => {
   return (
     
-<footer className="p-4 rounded-lg shadow md:px-6 md:py-8 bg-gray-900">
+<footer className="p-4 shadow md:px-6 md:py-8 bg-gray-900">
     <hr className="my-6 sm:mx-auto border-gray-700 lg:my-8" />
     <span className="block text-sm text-gray-500 sm:text-center dark:text-gray-400">© {new Date().getFullYear()  } <Link href={'/'} className="text-white hover:text-violet-400 hover:underline">dev book</Link>. All Rights Reserved.
     </span>
