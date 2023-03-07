1e461a3e2b92f7dc31ccb1047f9b2fc7878bcc99
Hamza Ali <hamzaali@Air-di-Hamza.lan>
2023-03-07 11:24:47 +0100

    final version for now update are neeeded

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -12,7 +12,7 @@ const BlogCard = ({ title, author, datePublished, slug, coverPhoto }) => {
       </Link>
       <div className="flex flex-col flex-1 p-6">
         <Link href={`/author/${author.name}`} className="text-xs tracking-wider uppercase hover:underline text-violet-400">{author.name}</Link>
-        <Link href={`/posts/${slug}`}><h3 className="flex-1 py-2 text-lg text-white font-semibold leading-snug">{title}</h3></Link>
+        <Link href={`/posts/${slug}`}><h3 className="flex-1 py-2 text-lg text-white text-opacity-80 font-bold transition-all duration-75 ease-in-out leading-snug lowercase hover:text-violet-500">{title}</h3></Link>
         <div className="flex flex-wrap justify-between pt-3 space-x-2 text-xs text-gray-400">
           <span>{moment(datePublished).format('MMMM Do YYYY')}</span>
           
