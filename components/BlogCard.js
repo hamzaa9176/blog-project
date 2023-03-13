488e0ed6d258e499c08b388cd004db247b19dda3
Hamza Ali <hamzaali@Air-di-Hamza.lan>
2023-03-13 23:30:19 +0100

    commit after des again

--- components/BlogCard.js
+++ components/BlogCard.js
@@ -1,6 +1,6 @@
 import moment from 'moment'
 import Link from "next/link";
-
+import Image from 'next/image';
 
 const BlogCard = ({ title, author, datePublished, slug, coverPhoto }) => {
   return (
@@ -15,7 +15,7 @@ const BlogCard = ({ title, author, datePublished, slug, coverPhoto }) => {
 
           <div className='flex flex-row justify-left w-full mt-5'>
             
-              {/* <img src={author.avatar.url} alt={author.name} className=" rounded-full w-10 h-10 object-cover" /> */}
+              <Image src={author.avatar.url} alt={author.name} width={10} height={10} className=" rounded-full w-10 h-10 object-cover" />
               <div className='flex flex-col ml-5'>
               <Link href={`/author/${author.name}`} className="text-md font-semibold lowercase hover:underline text-accent">{author.name}</Link>
             <span className='text-xs text-typing'>{moment(datePublished).format('MMMM Do YYYY')}</span>
